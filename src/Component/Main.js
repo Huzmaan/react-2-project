0ee668de45ef0a90ae5a9b0decc74e47ba319125
Huzmaan <princehuzmaanpasta@gmail.com>
2021-08-08 00:22:46 +0500

    main component change

--- src/Component/Main.js
+++ src/Component/Main.js
@@ -1,68 +1,61 @@
 import React from 'react';
-import { Card, ListGroupItem, ListGroup } from 'react-bootstrap'
-import Car from '../Assets/img/car.jpg';
-
+import {Card,Button} from 'react-bootstrap'
+import images from "../Assets/img/car.jpg"
+import images1 from "../Assets/img/car1.jpg"
 function Main({ }) {
     return (
         <>
-        <div style={{flexWrap="30px"}}>
+                <div style={{ display: 'flex', flexDirection: 'row', justifyContent:'space-evenly'}}>
         <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={Car} />
-                <Card.Body>
-                    <Card.Title>A-Class</Card.Title>
-                    <Card.Text>
-                    He Mercedes-Benz A-Class is a subcompact executive car (regular subcompact in its first two generations) produced by the German automobile 
-                    manufacturer Mercedes-Benz. The first generation (W168) was introduced in 1997, the second generation model (W169) appeared in late 2004 
-                    and the third generation model (W176) was launched in 2012. The fourth generation model (W177), which was launched in 2018, will mark the 
-                    first time the A-Class is offered in the United States and Canada.[1] This fourth generation A-Class will also be the first to be offered both 
-                    as a hatchback (W177) and sedan (V177).[2]
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem> <b>Price $50000</b></ListGroupItem>
-                    <ListGroupItem>0 Meter</ListGroupItem>
-                    <ListGroupItem>Manufacturer	
-                                   Daimler-Benz (1997–1998)
-                                   DaimlerChrysler (1998–2007)
-                                   Daimler AG (2007–present)
-                                   Production	1997–present</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={images} />
+  <Card.Body className="cardbody">
+    <Card.Title> <b> SLC 300 </b></Card.Title>
+    <Card.Text>
+      <b>Seating 2 person <br/>
+        HorsePower 241hp <br/>
+        MPG 23/32 <br/>
+        <h5>Starting at<br/>$49,950</h5>
+      </b>
+    </Card.Text>
+    <Button  href="https://www.google.com/"variant="primary">Go To More Detail</Button>
+  </Card.Body>
+</Card>
         </div>
 
         <div>
-            <Card style={{ width: '18rem' }}>
-                <Card.Img variant="top" src={Car} />
-                <Card.Body>
-                    <Card.Title>A-Class</Card.Title>
-                    <Card.Text>
-                    He Mercedes-Benz A-Class is a subcompact executive car (regular subcompact in its first two generations) produced by the German automobile 
-                    manufacturer Mercedes-Benz. The first generation (W168) was introduced in 1997, the second generation model (W169) appeared in late 2004 
-                    and the third generation model (W176) was launched in 2012. The fourth generation model (W177), which was launched in 2018, will mark the 
-                    first time the A-Class is offered in the United States and Canada.[1] This fourth generation A-Class will also be the first to be offered both 
-                    as a hatchback (W177) and sedan (V177).[2]
-                    </Card.Text>
-                </Card.Body>
-                <ListGroup className="list-group-flush">
-                    <ListGroupItem> <b>Price $50000</b></ListGroupItem>
-                    <ListGroupItem>0 Meter</ListGroupItem>
-                    <ListGroupItem>Manufacturer	
-                                   Daimler-Benz (1997–1998)
-                                   DaimlerChrysler (1998–2007)
-                                   Daimler AG (2007–present)
-                                   Production	1997–present</ListGroupItem>
-                </ListGroup>
-                <Card.Body>
-                    <Card.Link href="#">Card Link</Card.Link>
-                    <Card.Link href="#">Another Link</Card.Link>
-                </Card.Body>
-            </Card>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={images1} height="215rem" />
+  <Card.Body className="cardbody">
+    <Card.Title> <b> SLC43 AMG </b></Card.Title>
+    <Card.Text>
+      <b>Seating 2 person <br/>
+        HorsePower 241hp <br/>
+        MPG 23/32 <br/>
+        <h5>Starting at<br/>$49,950</h5>
+      </b>
+    </Card.Text>
+    <Button  href="https://www.google.com/"variant="primary">Go To More Detail</Button>
+  </Card.Body>
+</Card>
         </div>
+        <div>
+        <Card style={{ width: '18rem' }}>
+  <Card.Img variant="top" src={images1} height="215rem" />
+  <Card.Body className="cardbody">
+    <Card.Title> <b> SLC43 AMG </b></Card.Title>
+    <Card.Text>
+      <b>Seating 2 person <br/>
+        HorsePower 241hp <br/>
+        MPG 23/32 <br/>
+        <h5>Starting at<br/>$49,950</h5>
+      </b>
+    </Card.Text>
+    <Button  href="https://www.google.com/"variant="primary">Go To More Detail</Button>
+  </Card.Body>
+</Card>
+        </div>
+        
         </div>
      </> 
     )
